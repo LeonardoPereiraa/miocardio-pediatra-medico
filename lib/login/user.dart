2912c435da45ac233c7cbe217f23aafd18b79e24
Lucas Lopes <lucascastro@id.uff.br>
2019-11-02 11:24:26 -0300

    Started to work on Medico model and User class

---
+++ lib/login/user.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class User {
+  final FirebaseAuth _firebaseAuth;
+
+  User({FirebaseAuth firebaseAuth}) :
+        _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+  Future<void> signInWithCredentials(String email, String password) {
+    return _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signUp({String email, String password}) async {
+    return await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+}
+
